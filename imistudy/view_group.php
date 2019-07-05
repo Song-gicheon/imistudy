a2d0173e235787fa8f3198572c6abcb6068e43c9
Song-gicheon <gc1135@gmail.com>
2019-07-05 18:02:00 +0900

    add group & error fix

--- imistudy/view_group.php
+++ imistudy/view_group.php
@@ -1,16 +1,14 @@
 <?php
-
- 
  $sql = "select team_name from team where user_id='$id' group by team_name;";
 
  $result = $db->execute($sql);
  $k = 0;
  while(!$result->EOF){
 	 echo "<h3>".$result->fields[0]; // ÆÀ¸í
-	 echo " &nbsp; <button>Select Group</button>";
-	 echo " &nbsp; <button>Update Group</button>";
-	 echo " &nbsp; <button>Delete Group</button></h3>";
+	 echo " &nbsp; <button onclick='javascript:location.href=\"select_group.php?team=".$result->fields[0]."\"'>Select Group</button>";
+	 echo " &nbsp; <button onclick='javascript:location.href=\"update_group.php?team=".$result->fields[0]."\"'>Update Group</button>";
+	 echo " &nbsp; <button onclick='javascript:location.href=\"delete_group.php?team=".$result->fields[0]."\"'>Delete Group</button></h3>";
 	 $result->moveNext();
  }
- echo "<button>Make Group</button>  ";
+ echo "<button onclick='javascript:location.href=\"make_group.php\"'>Make Group</button>  ";
 ?>
