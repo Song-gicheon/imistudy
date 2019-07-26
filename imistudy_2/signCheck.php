1a41eb39fb012da1f35fdc5c61b6670c736d0d8e
Song-gicheon <gc1135@gmail.com>
2019-07-26 16:31:32 +0900

    add

--- imistudy_2/signCheck.php
+++ imistudy_2/signCheck.php
@@ -8,18 +8,21 @@
 
 
 	include($_SERVER['DOCUMENT_ROOT']."/imistudy/imistudy_2/DBcon.inc.php");
+
 	$db->execute("start transaction;"); // 여기서는 의미 없지만 통계 추정 테이블 이용시에 의미있음.
 
 	$sql = "INSERT INTO user VALUES('".$member_id."', '".$member_pw."', '".$member_name."', 0, ".$member_age.", ".$member_gender.");";
-
+	$rs = $db->execute($sql);
 	if($rs == false){
 		echo"
 		<script>
 			alert('이미 존재하는 아이디 입니다.');
 			history.back();
 		</script>
-		"
+		";
 	}
+	$db->execute("commit;");
+	unset($rs);
 	header("Location:index.php");
 
 ?>
